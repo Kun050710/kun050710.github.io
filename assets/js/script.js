f2dcf3def7f26c9891e6abf1d337d3d16287acdf
kun05 <87914017+Kun050710@users.noreply.github.com>
2023-12-13 22:14:44 +0100

    Removed the language button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// language button
-function changeLanguage(lang) {
-  document.documentElement.lang = lang;
-}
-
-// element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
-
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
